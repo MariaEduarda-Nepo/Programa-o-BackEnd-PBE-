cf4472e6169930ae7e07601ae13f5f1f3b7de209
MariaEduarda-Nepo <mariaeduardan995@gmail.com>
2025-08-13 14:13:38 -0300

    ..........

--- exercicioextraaula3/exercicioextra.php
+++ exercicioextraaula3/exercicioextra.php
@@ -1,31 +1,17 @@
 <?php
-// Lê o primeiro valor do usuário
-$valor1 = readline("Digite o primeiro valor: ");
 
-// Lê o segundo valor do usuário
+// Lê os 2 valores digitados pelo usuário via terminal
+$valor1 = readline("Digite o primeiro valor: ");
 $valor2 = readline("Digite o segundo valor: ");
 
-// Função para detectar o tipo do valor digitado
-function detectarTipo($valor) {
-    if (is_numeric($valor)) {
-        if (strpos($valor, '.') !== false) {
-            return 'float';
-        } else {
-            return 'integer';
-        }
-    } elseif (strtolower($valor) === 'true' || strtolower($valor) === 'false') {
-        return 'boolean';
-    } else {
-        return 'string';
-    }
-}
+// Interpreta o tipo de variável que foi digitado
+$tipo1 = gettype($valor1);
+$tipo2 = gettype($valor2);
 
-$tipo1 = detectarTipo($valor1);
-$tipo2 = detectarTipo($valor2);
-
-if ($tipo1 === $tipo2) {
-    echo "Variáveis de tipos iguais! Primeiro valor do tipo {$tipo1} e segundo valor do tipo {$tipo2}\n";
+// Compara os tipos
+if ($tipo1 == $tipo2) {
+    echo "Variáveis de tipos iguais! Primeiro valor do tipo {$tipo1} e segundo valor do tipo {$tipo2}.\n";
 } else {
-    echo "ERRO! Variáveis de tipos diferentes. Primeiro valor do tipo {$tipo1} e segundo valor do tipo {$tipo2}\n";
+    echo "ERRO! Variáveis de tipos diferentes. Primeiro valor do tipo {$tipo1} e segundo valor do tipo {$tipo2}.\n";
 }
 ?>
