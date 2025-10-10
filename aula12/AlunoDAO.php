0b6dffcfd50f23dbdf560cbe76230d9482e16f71
MariaEduarda-Nepo <mariaeduardan995@gmail.com>
2025-10-10 16:54:14 -0300

    //

---
+++ aula12/AlunoDAO.php
@@ -0,0 +1,25 @@
+<?php
+
+class AlunoDAO {
+    private $alunos = [];
+
+    public function criarAluno()
+    {
+
+    }
+
+    public function lerAluno()
+    {
+
+    }
+
+    public function atualizarAluno()
+    {
+
+    }
+    public function excluirAluno()
+    {
+
+    }
+
+}
